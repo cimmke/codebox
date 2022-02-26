e6872658bc062796a61014f90e92a810a0f28aff
Corey Immke <cimmke@protonmail.com>
2022-02-26 18:19:40 -0500

    Create api app and visitor model

---
+++ api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Visitor(models.Model):
+    SOURCE_CONTROL_CHOICES = [
+        ('github', 'GitHub'),
+        ('gitlab', 'GitLab'),
+        ('bitbucket', 'BitBucket'),
+        ('tfs', 'TFS'),
+        ('other', 'Other'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    source_control = models.CharField(
+        choices=SOURCE_CONTROL_CHOICES,
+        default='other',
+        max_length=max(len(s[0]) for s in SOURCE_CONTROL_CHOICES)
+    )
+    team_size = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
